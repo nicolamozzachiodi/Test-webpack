3bc8dab35d3439248aeb50ff8da6b74354ac4c0b
Nicola Mozzachiodi <43537327+nicolamozzachiodi@users.noreply.github.com>
2020-01-28 17:05:50 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+const webpack = require('webpack-stream');
+
+gulp.task('scripts', function() {
+  return gulp.src('./src/js/index.js')
+    .pipe(webpack({
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        }
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass({
+            includePaths: ['node_modules/slick-carousel/slick']
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.js', gulp.parallel('scripts'));
+    gulp.watch('src/**/*.scss', gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('scripts', 'styles'))
